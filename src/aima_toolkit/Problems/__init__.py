bd5bb3722400fe80f2e1c2f79a53d6a148d96b77
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-09-01 17:20:11 +0200

    Make the __init__.py more descriptive for ease of use

---
+++ src/aima_toolkit/Problems/__init__.py
@@ -0,0 +1,4 @@
+from eight_queen_problem import EightQueenProblem
+from romania_search_problem import Romania_Search_Problem, Romania_Search_Problem_Uniform_Cost
+
+__all__ = ["EightQueenProblem", "Romania_Search_Problem", "Romania_Search_Problem_Uniform_Cost"]
