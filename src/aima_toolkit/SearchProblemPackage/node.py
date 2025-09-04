378a6b05d5c1e12bcc9b6e77cf24722727cc2914
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-09-04 20:30:18 +0200

    Add better type hints to the Node class

--- src/aima_toolkit/SearchProblemPackage/node.py
+++ src/aima_toolkit/SearchProblemPackage/node.py
@@ -1,21 +1,23 @@
-class Node():
-  def __init__(self, state, parent = None, path_cost = 0, action = None):
+from typing import Self
+
+class Node[S,A]:
+  def __init__(self, state : S, parent : Self | None = None, path_cost : float | int = 0, action : A | None = None):
     self.state = state
     self.parent = parent
     self.path_cost = path_cost
     self.depth = 0 if parent is None else parent.depth + 1
     self.action = action
 
-  def __str__(self):
+  def __str__(self) -> str:
     return self.__repr__()
 
   def __eq__(self, value: object) -> bool:
     return self.state == value.state if isinstance(value, Node) else False
 
-  def __repr__(self):
+  def __repr__(self) -> str:
     return f"Node({self.state}, cost={self.path_cost})"
 
-  def get_path(self):
+  def get_path(self) -> list[S]:
     path = []
     current = self
     while current:
@@ -24,7 +26,7 @@ class Node():
 
     return path[::-1]
   
-  def get_actions(self):
+  def get_actions(self) -> list[A]:
     actions = []
     current = self
     while current:
