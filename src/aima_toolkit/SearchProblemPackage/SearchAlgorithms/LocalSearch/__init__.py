f72bc0088fe403fd6e7eed13201768af1f7227c8
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-09-07 13:16:28 +0200

    Changge the LocalSearch package to also make the __init__ independent of local __all__ rather for it to use the __all__ of its modules

--- src/aima_toolkit/SearchProblemPackage/SearchAlgorithms/LocalSearch/__init__.py
+++ src/aima_toolkit/SearchProblemPackage/SearchAlgorithms/LocalSearch/__init__.py
@@ -1,7 +1,5 @@
-from .genetic_algorithm import genetic_algorithm_search
-from .simulated_annealing import simulated_annealing, logarith_scheduler, linear_scheduler, geometric_scheduler
-from .hill_climbing import hill_climbing_search
-from .local_beam import local_beam_search
-from .stochastic_hill_climbing import random_restart_hill_climbing_search, first_choice_hill_climbing_search
-
-__all__ = ["genetic_algorithm_search", "simulated_annealing", "hill_climbing_search", "local_beam_search", "random_restart_hill_climbing_search", "first_choice_hill_climbing_search"]
+from .genetic_algorithm import *
+from .simulated_annealing import *
+from .hill_climbing import *
+from .local_beam import *
+from .stochastic_hill_climbing import *
