028b82e341c1ad8cf9dc42a6cc1b5ac9e9e2b0dc
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-09-08 21:35:12 +0200

    Do some changed to type hints, mainly the returning of frozensets

--- src/aima_toolkit/SearchProblemPackage/SearchAlgorithms/PartiallyObservableSearch/partially_observable_problem.py
+++ src/aima_toolkit/SearchProblemPackage/SearchAlgorithms/PartiallyObservableSearch/partially_observable_problem.py
@@ -3,59 +3,67 @@ from abc import ABC, abstractmethod
 
 from ... import SearchProblem
 
-class PartiallyObservableProblem[S,A,P](ABC, SearchProblem):
+class PartiallyObservableProblem[S,A,P](SearchProblem, ABC):
   @abstractmethod
-  def PERCEPT(self, state) -> None | P: # Returns None for sensorless, returns the state for observable, or it returns a percept describing the state
+  def PERCEPT(self, state) -> P: # Returns None for sensorless, returns the state for observable, or it returns a percept describing the state
     raise NotImplementedError
 
   @abstractmethod
-  def PREDICT(self, state : S, action : A) -> set[S]: # Equivalent to RESULTSp
+  def PREDICT(self, state : S, action : A) -> frozenset[S]: # Equivalent to RESULTSp
     raise NotImplementedError
 
   @abstractmethod
-  def ACTIONSp(self, state : S) -> set[A]:
+  def ACTIONSp(self, state : S) -> frozenset[A]:
     raise NotImplementedError
 
   @abstractmethod
   def ACTION_COSTp(self, state : S, action : A, new_state : S) -> float:
     raise NotImplementedError
 
-  def POSSIBLE_PERCEPTS(self, belief_state : set[S]) -> set[P | None]:
-    possible_percepts : set[P] = set()
-    for o in (self.PERCEPT(state) for state in belief_state):
-      possible_percepts.add(o)
+  @abstractmethod
+  def IS_GOALp(self, state : S) -> bool:
+    raise NotImplementedError
 
-    return possible_percepts
+  def POSSIBLE_PERCEPTS(self, state: frozenset[ S ]) -> frozenset[ P ]:
+    return frozenset( self.PERCEPT(s) for s in state )
 
-  def UPDATE(self, belief_state : set[S], percept : P) -> set[S]:
-    return {state for state in belief_state if percept == self.PERCEPT(state)}
+  def __init__(self, initial_belief_state : frozenset[S], allow_illegal_actions : bool = False) -> None:
+    super().__init__(initial_belief_state)
+    self.allow_illegal_actions = allow_illegal_actions
 
-  def RESULTS(self, belief_state : set[S], action : A) -> Iterable[set[S]]:
-    new_belief_state : set[S] = set().union(*(self.PREDICT(state=state,action=action) for state in belief_state))
-    for percept in self.POSSIBLE_PERCEPTS(belief_state=new_belief_state):
+  def IS_GOAL(self, state: frozenset[ S ]) -> bool:
+    return all( self.IS_GOALp(concrete_state) for concrete_state in state )
+
+  def UPDATE(self, state : frozenset[S ], percept : P) -> frozenset[ S ]:
+    return frozenset(s for s in state if percept == self.PERCEPT( s ))
+
+  def RESULTS(self, state : set[ S ], action : A) -> Iterable[ frozenset[ S ] ]:
+    new_belief_state : frozenset[S] = frozenset( s_new for s in state for s_new in self.PREDICT(state=s, action=action))
+    for percept in self.POSSIBLE_PERCEPTS( state=frozenset(new_belief_state) ):
       yield self.UPDATE(new_belief_state, percept)
 
-  def ACTIONS(self, belief_state : set[S], allow_illegal_actions: bool = False) -> set[A]:
-    actions = self.ACTIONSp( state=belief_state.pop( ) )
-    for state in belief_state:
-      if allow_illegal_actions:
-        actions |= self.ACTIONS( state )
+  def ACTIONS(self, state : frozenset[ S ]) -> frozenset[ A ]:
+    state = set(state)
+    actions : set[A] = set(self.ACTIONSp( state=state.pop( ) ))
+    for s in state:
+      if self.allow_illegal_actions:
+        actions |= self.ACTIONSp( s )
       else:
-        actions &= self.ACTIONS( state )
+        actions &= self.ACTIONSp( s )
 
-    return actions
+    return frozenset(actions)
 
-  def ACTION_COST(self, belief_state : set[S], action : A, new_belief_state : set[S]) -> float:
+  def ACTION_COST(self, state : frozenset[ S ], action : A, new_state : frozenset[ S ]) -> float:
     costs = (
-        self.ACTION_COSTp(state=state, action=action, new_state=new_state)
-        for state in belief_state
-        for new_state in self.PREDICT(state=state, action=action)
-        if new_state in new_belief_state
+        self.ACTION_COSTp(state=concrete_state, action=action, new_state=new_state)
+        for concrete_state in state
+        for predicted_new_state in self.PREDICT(state=concrete_state, action=action)
+        if predicted_new_state in new_state
     )
 
     return max(costs, default=float("inf"))
 
-  def EVALUATION_SCORE(self, belief_state : set[S], action : A, new_belief_state : set[S], heuristic : Callable[[S], float]) -> float:
+  def EVALUATION_SCORE(self, belief_state : frozenset[S], action : A, new_belief_state : frozenset[S], heuristic : Callable[[S], float]) -> float:
     evaluation_score = (
         self.ACTION_COSTp(state=state, action=action, new_state=new_state) + heuristic(state)
         for state in belief_state
