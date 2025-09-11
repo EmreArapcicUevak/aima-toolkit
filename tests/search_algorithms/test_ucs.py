d04016b3d5239f944ebf776a8bf051df5504c6e0
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-09-11 12:42:09 +0200

    Change the tests so they work for the new functon definitions

--- tests/search_algorithms/test_ucs.py
+++ tests/search_algorithms/test_ucs.py
@@ -4,26 +4,29 @@ from src.aima_toolkit.SearchProblemPackage import SearchStatus
 
 class TestSmallestCost():
   def test_arad_to_giurgiu(self):
-    result_node = uniform_cost_search(Romania_Search_Problem(initial_state="Arad", goal_state="Giurgiu"))
+    search_status, result_node = uniform_cost_search(Romania_Search_Problem(initial_state="Arad", goal_state="Giurgiu"))
+    assert search_status == SearchStatus.SUCCESS
     list_of_states = result_node.get_path() if result_node else []
     assert list_of_states == ["Arad", "Sibiu", "Rimnicu Vilcea", "Pitesti", "Bucharest", "Giurgiu"]
 
   def test_sibiu_to_bucharest(self):
-    result_node = uniform_cost_search(Romania_Search_Problem(initial_state="Sibiu", goal_state="Bucharest"))
+    search_status, result_node = uniform_cost_search(Romania_Search_Problem(initial_state="Sibiu", goal_state="Bucharest"))
     list_of_states = result_node.get_path() if result_node else []
     assert list_of_states == ["Sibiu", "Rimnicu Vilcea", "Pitesti", "Bucharest"]
 
 class TestSameCost():
   def test_arad_to_giurgiu(self):
-    result_node = uniform_cost_search(Romania_Search_Problem_Uniform_Cost(initial_state="Arad", goal_state="Giurgiu"))
+    search_status, result_node = uniform_cost_search(Romania_Search_Problem_Uniform_Cost(initial_state="Arad", goal_state="Giurgiu"))
+    assert search_status == SearchStatus.SUCCESS
     list_of_states = result_node.get_path() if result_node else []
     assert list_of_states == ["Arad", "Sibiu", "Fagaras", "Bucharest","Giurgiu"]
 
   def test_arad_to_arad(self):
-    result_node = uniform_cost_search(Romania_Search_Problem_Uniform_Cost(initial_state="Arad", goal_state="Arad"))
+    search_status, result_node = uniform_cost_search(Romania_Search_Problem_Uniform_Cost(initial_state="Arad", goal_state="Arad"))
+    assert search_status == SearchStatus.SUCCESS
     list_of_states = result_node.get_path() if result_node else []
     assert list_of_states == ["Arad"]
 
   def test_arad_to_nothing(self):
-    result_node = uniform_cost_search(Romania_Search_Problem_Uniform_Cost(initial_state="Arad", goal_state="Nothing"))
-    assert result_node == SearchStatus.FAILURE
+    search_status, result_node = uniform_cost_search(Romania_Search_Problem_Uniform_Cost(initial_state="Arad", goal_state="Nothing"))
+    assert search_status == SearchStatus.FAILURE
