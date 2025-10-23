a7786623d371494ccffde549813681775e46393f
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-10-23 18:07:46 +0200

    Add a method to add the alldiff constraint as a set of binary constraints

--- src/aima_toolkit/ConstraintSatisfactionProblemPackage/PremadeConstraints/alldiff_constraint.py
+++ src/aima_toolkit/ConstraintSatisfactionProblemPackage/PremadeConstraints/alldiff_constraint.py
@@ -8,7 +8,16 @@ def alldiff_constraint_func(assignment : dict[str, Any]) -> bool:
 def get_all_diff_constraint(vars : set[str]):
   return Constraint(list(vars), alldiff_constraint_func)
 
+def add_alldiff_constraint_as_binary_constraint(csp : ConstraintSatisfactionProblem, vars : set[str]):
+  assert all(var in csp.variables for var in vars)
+  already_seen_vars : set[str] = set()
+  for var1 in vars:
+    already_seen_vars.add(var1)
+    for var2 in vars - already_seen_vars:
+      csp.add_constraint(Constraint([var1, var2], alldiff_constraint_func))
+
 def alldiff_constraint_propagation(csp : ConstraintSatisfactionProblem, vars : set[str]):
+  assert all(var in csp.variables for var in vars)
   all_values = set()
   remaining_vars = copy.deepcopy(vars)
   domain_copy = copy.deepcopy(csp.domains)
@@ -32,4 +41,4 @@ def alldiff_constraint_propagation(csp : ConstraintSatisfactionProblem, vars : s
 
   return len(remaining_vars) <= len(all_values)
 
-__all__ = ['alldiff_constraint_propagation', 'get_all_diff_constraint']
+__all__ = ['alldiff_constraint_propagation', 'get_all_diff_constraint', 'add_alldiff_constraint_as_binary_constraint']
